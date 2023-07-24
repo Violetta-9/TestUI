df44c07fab4d54cb04279133e6f3e0b550508acf
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-07-24 15:43:30 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { UsMapComponent } from './us-map/us-map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {BASE_PATH, StatesService} from "./core/services/swagger-gen/service";
+import {environment} from "../enviroment/enviroment";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsMapComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    StatesService,{
+      provide: BASE_PATH,
+      useValue: environment.testURI,
+    },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
